f6a090454ba0bfbfd17085343f30a6aae446f3c0
andresnino1 <andresnino@gmail.com>
2020-10-12 12:36:01 +1100

    adding home page and about page

--- mypage/urls.py
+++ mypage/urls.py
@@ -1,6 +1,9 @@
+#mypage / urls.py
+
 from django.urls import path
-from .views import homepageview
+from .views import HomePageView, AboutPageView
 
 urlpatterns = [
-    path('', homepageview, name="home"),
-]
+    path('', HomePageView.as_view(), name="home"),
+    path('about/', AboutPageView.as_view(), name="about"),
+   ]
